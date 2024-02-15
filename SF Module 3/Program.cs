d31933a47929ccbd970934054c3f424731c5ffd8
houteqwe <hout3e123@gmail.com>
2024-02-15 07:55:16 +0500

    Добавьте файлы проекта.

---
+++ SF Module 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Test";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
