b321774e130513952a891b7b91ce13c3615103e3
houteqwe <hout3e123@gmail.com>
2024-02-15 08:23:32 +0500

    Task 3.2.8

--- SF Module 3/Program.cs
+++ SF Module 3/Program.cs
@@ -6,9 +6,13 @@ class MainClass
     {
 
         const string MyName = "Jane";
-
         Console.WriteLine(MyName);
 
+        Console.WriteLine("\t Привет мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n Jane");
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\u0023");
         Console.ReadKey();
     }
 }
