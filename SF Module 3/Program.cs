5729ba6fe6a0e8431777cffc78c6887d83aa6f71
houteqwe <hout3e123@gmail.com>
2024-02-15 10:29:09 +0500

    added task 3.3.2 .cs , Program.cs returned to previous ver

--- SF Module 3/Program.cs
+++ SF Module 3/Program.cs
@@ -5,15 +5,14 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
+        const string MyName = "Jane";
+        Console.WriteLine(MyName);
 
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Console.WriteLine("\t Привет мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n Jane");
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\u0023");
+        Console.ReadKey();
     }
 }
