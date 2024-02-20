e46f87abc97068afc18545d2b130f8b52b32b8f4
houteqwe <hout3e123@gmail.com>
2024-02-21 00:26:46 +0500

    added Module 3 final task

---
+++ SF Module 3/Module 3 final task/Module 3 Final Task.cs
@@ -0,0 +1,16 @@
+﻿class FinalTask
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Enter your name: ");
+        var name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("Enter your birthdate: ");
+        var birthdate = Console.ReadLine();
+        Console.WriteLine("Your birthdate is: {0}", birthdate);
+        Console.ReadKey();
+    }
+}
