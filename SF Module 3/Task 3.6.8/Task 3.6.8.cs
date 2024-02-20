d1c530ee99b16a548e1b81ddb7170109782dfddc
houteqwe <hout3e123@gmail.com>
2024-02-20 23:37:16 +0500

    added task 3.6.8

---
+++ SF Module 3/Task 3.6.8/Task 3.6.8.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Task368
+{
+    public static void Main(string[] args)
+    {
+        double result = 10 % 3;
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+}
