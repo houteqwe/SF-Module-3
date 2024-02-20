e641658af9d45a1327e9b3435b6274f7b0b4dbc3
houteqwe <hout3e123@gmail.com>
2024-02-20 21:12:25 +0500

    added DaysOfWeek.cs deleted Program.cs

---
+++ SF Module 3/DaysOfWeek.cs
@@ -0,0 +1,25 @@
+﻿namespace SF_Module_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek MyFavoriteDay;
+
+            MyFavoriteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFavoriteDay);
+        }
+    }
+
+}
+enum DaysOfWeek : byte
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
