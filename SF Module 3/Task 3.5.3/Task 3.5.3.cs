14f5a89c0ee6b53121edc85681c72690908248ec
houteqwe <hout3e123@gmail.com>
2024-02-20 22:11:18 +0500

    added task 3.5.3

---
+++ SF Module 3/Task 3.5.3/Task 3.5.3.cs
@@ -0,0 +1,18 @@
+﻿namespace Task353
+{
+    class Task353
+    {
+        public static void Main(string[] args)
+        {
+            string MyName = "Игорь";
+            string age = "18";
+            Console.WriteLine("Привет, Мир");
+            Console.WriteLine();
+            Console.WriteLine($"Меня зовут {MyName}");
+            Console.WriteLine();
+            Console.WriteLine($"Мой возраст {age}");
+
+            Console.ReadKey();
+        }
+    }
+}
