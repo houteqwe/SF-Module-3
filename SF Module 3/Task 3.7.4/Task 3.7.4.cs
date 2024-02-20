ba0dfe2bc10c9a80b0da44bc1da25b66175eed14
houteqwe <hout3e123@gmail.com>
2024-02-21 00:10:27 +0500

    added task 3.7.4

---
+++ SF Module 3/Task 3.7.4/Task 3.7.4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Task374
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.Write("What is your favorite day of week?");
+        DayOfWeek day = (DayOfWeek) int.Parse (Console.ReadLine());
+        Console.WriteLine("Your favorite day of week is {0}", day);
+        Console.ReadKey ();
+    }
+}
+
+
+                
